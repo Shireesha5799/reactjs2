8995ea443db40e7dd92d85ef7d3c7775ffb2deaa
Shireesha5799 <shireesha5799@gmail.com>
2021-06-01 15:13:11 +0530

    This is git demo

---
+++ src/com/capg/git/MyClass.java
@@ -0,0 +1,13 @@
+package com.capg.git;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heloo");
+		System.out.println("This is get demo");
+		System.out.println("Welcome");
+
+	}
+
+}
